c92bdecedfa79e505c4970af9078f1ea434dad31
Cesar Erebus <cerebus@at-g5.msk.21-school.ru>
2020-11-23 17:27:07 +0300

    I've rewritten after moulinette evalustion ft_split.c and ft_strtrim.c

--- ft_split.c
+++ ft_split.c
@@ -3,34 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cerebus <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: cerebus <cerebus@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 19:34:13 by cerebus           #+#    #+#             */
-/*   Updated: 2020/11/15 19:34:17 by cerebus          ###   ########.fr       */
+/*   Updated: 2020/11/23 17:23:22 by cerebus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
-static size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t i;
-
-	i = 0;
-	if (!dst || !src)
-		return (0);
-	if (dstsize == 0)
-		return (0);
-	while ((i < dstsize - 1) && (src[i] != '\0'))
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (0);
-}
-
-static int		ft_count_words(char const *s, char c)
+static int	ft_count_words(char const *s, char c)
 {
 	int i;
 	int words_num;
@@ -49,29 +32,25 @@ static int		ft_count_words(char const *s, char c)
 	return (words_num);
 }
 
-static char		**ft_get_mem(int arr_num, char **res, int start, int end)
+static char	**ft_free(int arr_num, char **res)
 {
-	res[arr_num] = (char *)malloc((end - start + 2) * sizeof(char));
-	if (!res[arr_num])
+	arr_num = arr_num - 1;
+	while (arr_num >= 0)
 	{
-		arr_num = arr_num - 1;
-		while (arr_num >= 0)
-		{
-			free(res[arr_num]);
-			arr_num--;
-		}
-		free(res);
-		return (NULL);
+		free(res[arr_num]);
+		arr_num--;
 	}
-	return (res);
+	free(res);
+	return (NULL);
 }
 
-static char		**ft_fill_res(char **res, char const *s, char c, int words_num)
+static char	**ft_fill_res(char **res, char const *s, char c,
+									unsigned int words_num)
 {
-	int i;
-	int start;
-	int end;
-	int arr_num;
+	unsigned int i;
+	unsigned int start;
+	unsigned int end;
+	unsigned int arr_num;
 
 	i = 0;
 	start = 0;
@@ -85,24 +64,24 @@ static char		**ft_fill_res(char **res, char const *s, char c, int words_num)
 		while (s[i] != c && s[i] != '\0')
 			i++;
 		end = i - 1;
-		if (!ft_get_mem(arr_num, res, start, end))
-			return (NULL);
-		ft_strlcpy(res[arr_num], &s[start], end - start + 2);
+		res[arr_num] = ft_substr(s, start, (end - start + 1));
+		if (!(res[arr_num]))
+			return (ft_free(arr_num, res));
 		arr_num++;
 	}
 	res[arr_num] = NULL;
 	return (res);
 }
 
-char			**ft_split(char const *s, char c)
+char		**ft_split(char const *s, char c)
 {
-	int		words_num;
-	char	**res;
+	unsigned int	words_num;
+	char			**res;
 
 	if (!s || !c)
 		return (NULL);
 	words_num = ft_count_words(s, c);
-	res = (char **)malloc((words_num + 1) * sizeof(char **));
+	res = (char **)malloc((words_num + 1) * sizeof(char *));
 	if (!res)
 		return (NULL);
 	return (ft_fill_res(res, s, c, words_num));
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,35 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cerebus <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: cerebus <cerebus@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 17:09:39 by cerebus           #+#    #+#             */
-/*   Updated: 2020/11/15 17:09:54 by cerebus          ###   ########.fr       */
+/*   Updated: 2020/11/23 16:15:33 by cerebus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
-char	*ft_strtrim(char const *s1, char const *set)
+static	int	ft_compare(char check_ch, char const *set)
 {
-	char		*res;
-	size_t		start;
-	size_t		end;
+	int i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (check_ch == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char		*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
 
 	if (!s1)
 		return (NULL);
 	if (!set)
 		return (ft_strdup(s1));
 	start = 0;
-	end = ft_strlen(s1);
-	while (s1[start] != '\0' && ft_memchr(set, s1[start], ft_strlen(set)))
+	end = ft_strlen(s1) - 1;
+	while (ft_compare(s1[start], set) && s1[start] != '\0')
 		start++;
-	while (end > start && ft_memchr(set, s1[end], ft_strlen(s1)))
+	if (!s1[start])
+		return (ft_strdup("\0"));
+	while (ft_compare(s1[end], set) && (end > 0))
 		end--;
-	res = malloc((end - start + 2) * sizeof(char));
-	if (!res)
-		return (NULL);
-	ft_strlcpy(res, &s1[start], (end - start + 2));
-	return (res);
+	return (ft_substr(s1, start, end - start + 1));
 }
